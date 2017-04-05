0a5ba69600f21aa1de39e4aaf0793d98f04188fe
Sudarshan <sudarshansk14@gmail.com>
2017-04-05 19:01:11 +0530

    Binary splitting

--- RougeEval/rougetest.py
+++ RougeEval/rougetest.py
@@ -1,12 +1,15 @@
 from pyrouge import Rouge155
 
 r = Rouge155('/home/sudarshan/Downloads/RELEASE-1.5.5/')
-r.system_dir = 'syssum/'
-r.model_dir = 'modsum/'
-r.system_filename_pattern = 'bootest_(\d+).txt'
+r.system_dir = '/home/sudarshan/Desktop/TheChainRanker/RougeEval/syssum/suraj/'
+r.model_dir = '/home/sudarshan/Desktop/TheChainRanker/RougeEval/modsum/'
+r.system_filename_pattern = 'summary_(\d+).txt'
 #r.model_filename_pattern = 'bootest.[A-Z].#ID#.txt'
 r.model_filename_pattern = 'summary_#ID#.txt'
 
 output = r.convert_and_evaluate()
-print(output)
+f = open('respg.txt','w')
+f.write(output)
 output_dict = r.output_to_dict(output)
+print (output_dict)
+f.close()
