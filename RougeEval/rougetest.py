c040dbc4855518de661eb41176dab840946870d1
Sudarshan <sudarshansk14@gmail.com>
2017-04-05 20:14:03 +0530

    Done

--- RougeEval/rougetest.py
+++ RougeEval/rougetest.py
@@ -1,15 +1,17 @@
 from pyrouge import Rouge155
 
 r = Rouge155('/home/sudarshan/Downloads/RELEASE-1.5.5/')
-r.system_dir = '/home/sudarshan/Desktop/TheChainRanker/RougeEval/syssum/suraj/'
+r.system_dir = '/home/sudarshan/Desktop/TheChainRanker/RougeEval/modsum/'
 r.model_dir = '/home/sudarshan/Desktop/TheChainRanker/RougeEval/modsum/'
 r.system_filename_pattern = 'summary_(\d+).txt'
 #r.model_filename_pattern = 'bootest.[A-Z].#ID#.txt'
 r.model_filename_pattern = 'summary_#ID#.txt'
 
 output = r.convert_and_evaluate()
-f = open('respg.txt','w')
+
+f = open('restest.txt','w')
 f.write(output)
+
 output_dict = r.output_to_dict(output)
 print (output_dict)
-f.close()
+#f.close()
