a75331b590c9ccbc3fe3b8b5ad08605600920e13
Sudarshan <sudarshansk14@gmail.com>
2017-04-05 18:22:26 +0530

    Small changes

--- RougeEval/rougetest.py
+++ RougeEval/rougetest.py
@@ -3,7 +3,7 @@ from pyrouge import Rouge155
 r = Rouge155('/home/sudarshan/Downloads/RELEASE-1.5.5/')
 r.system_dir = 'syssum/'
 r.model_dir = 'modsum/'
-r.system_filename_pattern = 'bootest.(\d+).txt'
+r.system_filename_pattern = 'bootest_(\d+).txt'
 #r.model_filename_pattern = 'bootest.[A-Z].#ID#.txt'
 r.model_filename_pattern = 'summary_#ID#.txt'
 
